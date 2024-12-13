d1a8dd7ed0838a345ab2b7f947a3ec1d02a2e167
nati-d <nathnaeldes@gmail.com>
2024-12-13 19:01:24 +0300

    api changed

--- api/agents_api.tsx
+++ api/agents_api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://127.0.0.1:5000';
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 const fetchAgents = async () => {
   try {
