6200363f5e09c601449af4d7bd4b2f2d50441022
nati-d <nathnaeldes@gmail.com>
2024-12-17 16:57:43 +0300

    url updated

--- api/agents_api.tsx
+++ api/agents_api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "https://agents4hire-154919156893.us-central1.run.app";
 
 const fetchAgents = async () => {
   try {
